41353416cc72be9fedf796fd178381d03f4bb43e
lejosviak <163347879+lejosviak@users.noreply.github.com>
2024-10-21 09:25:08 -0300

    ;

---
+++ perguntas.js
@@ -0,0 +1,120 @@
+criaCartao( 
+    'Matemática',
+    'Qual é a fórmula para calcular a área de um triângulo?',
+    'A = b x h / 2'
+    )
+    
+    criaCartao( 
+    'História',
+     'Qual é a capital do Império Romano durante a maior parte da sua história?',
+    'Roma'
+    )
+    
+    criaCartao( 
+    'Física',
+    'O que é a Lei da Gravitação de Newton?',
+    'A força de atração entre dois corpos é diretamente proporcional ao produto de suas massas'
+    )
+    
+    criaCartao( 
+    'Biologia',
+    'Qual é a principal função dos ribossomos nas células?',
+    'Sintetizar proteínas'
+    )
+    
+    criaCartao( 
+    'Matemática',
+    'Qual é a equação geral da reta no plano cartesiano?',
+    'y = mx + b'
+    )
+    
+    criaCartao( 
+     'Língua Portuguesa',
+     'Qual é a principal obra de Machado de Assis que faz parte do movimento realista?',
+    'Dom Casmurro'
+    )
+    
+    criaCartao( 
+    'História',
+    'Em que ano foi proclamada a independência do Brasil?',
+    '1822'
+    )
+    
+    criaCartao( 
+    'Física',
+    'Qual é a fórmula da velocidade média?',
+    'v = s/t'
+    )
+    
+    criaCartao( 
+    'Biologia',
+    'Qual é o principal processo de divisão celular em organismos eucarióticos?',
+    'Mitose'
+    )
+    
+    criaCartao( 
+    'Química',
+    'Qual é o nome dado ao fenômeno de expansão dos gases quando a temperatura aumenta?',
+    'Lei de Charles'
+    )
+    
+    
+    criaCartao( 
+    'matemática',
+    'Qual é a raiz quadrada de 144?',
+    '12'
+    )
+    
+    criaCartao( 
+    'matemática',
+    'Qual é o valor de 3x+5=20',
+    'x=5.'
+    )
+    
+    criaCartao( 
+    'História',
+    'Em que ano aconteceu a Proclamação da República no Brasil?',
+     '1889.',
+    )
+    
+    criaCartao( 
+    'História',
+    'Qual foi o principal objetivo das Grandes Navegações do século XV?',
+    'Encontrar novas rotas comerciais para as Índias e expandir territórios.'
+    )
+    
+    criaCartao( 
+    'Geografia',
+    'Qual é o rio mais extenso do mundo?',
+    'Rio Amazonas.'
+    )
+    
+    criaCartao( 
+    'Geografia',
+    'Em que hemisfério se encontra a maior parte do continente africano?',
+     'Hemisfério Oriental.'
+    )
+    
+    criaCartao( 
+    'Ciências',
+    'Qual é o nome do processo pelo qual as plantas produzem seu próprio alimento usando luz solar?',
+    'Fotossíntese.'
+    )
+    
+    criaCartao( 
+    'Ciências',
+    'Qual é o gás mais abundante na atmosfera terrestre?',
+    'Nitrogênio.'
+    )
+    
+    criaCartao( 
+    'Língua Portuguesa',
+    'Qual é a função de um advérbio em uma frase?',
+    'Modificar o verbo, o adjetivo ou outro advérbio, indicando circunstâncias como tempo, lugar, modo, intensidade, etc.',
+    )
+    
+    criaCartao( 
+    'Língua Portuguesa',
+    'Qual é o objeto direto na frase: "Maria leu o livro rapidamente"?',
+    'o livro'
+    )
