857ed27533a09d86c12d3cdb81cd58c11030898b
lejosviak <163347879+lejosviak@users.noreply.github.com>
2024-10-21 10:13:56 -0300

    d

--- mains.js
+++ mains.js
@@ -9,7 +9,7 @@ function criaCartao (categoria, pergunta, resposta){
         <p> ${pergunta}</p>
     </div>
         <div class="respostas-cartao"></div>
-        <p> ${resposta}</p>
+        <p>${resposta}</p>
     </div>
     </div>
     `
